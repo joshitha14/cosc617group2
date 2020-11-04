857a7d0af9b307bb3940b00760733a68afbe2178
Ian Woods <iwoods@DESKTOP-IQIMCTS.localdomain>
2020-11-04 16:51:04 -0500

    Added method to meet.js for calculating age from birthdate.

--- doggy_date/src/meet.js
+++ doggy_date/src/meet.js
@@ -5,8 +5,7 @@ import heart from "./images/heart-solid.png"
 import cross from "./images/times-solid.svg";
 
 function Meet() { 
-  
-  //useEffect() will be used to load the user details.
+  //useEffect() will be used to call fetchUserDetails() when the component mounts. 
   //The [] argument tells useEffect() to run when the component mounts. 
   useEffect (() => {
     fetchUserDetails();
@@ -14,7 +13,7 @@ function Meet() {
 
   var [users, setUsers] = useState([]);
 
-  //Fetch user details and photo ID#s:
+  //Fetch user details and photo ID#s. Store in separate object for each user:
   const fetchUserDetails = async () => {
     const data = await fetch('http://localhost:3001/user_details/');
     const userDetails = await data.json();
@@ -23,8 +22,8 @@ function Meet() {
     for(var user of userDetails) {
       const data = await fetch(`http://localhost:3001/photos?Username=${user.Username}`);
       const imageFileNames = await data.json();
-      user.currentImage = 0; //This stores the index of the image that is currently displayed.
-      user.images = []; //This array will store the filenames for the images.
+      user.currentImage = 0; //This will store the index of the image that is currently displayed for the user.
+      user.images = []; //This array will store the filenames for the user's images.
       for(var file of imageFileNames) { 
         user.images.push(file.PhotoID); //Push the filenames for each image into the array. 
       }
@@ -32,15 +31,14 @@ function Meet() {
     setUsers(userDetails);
   }
   
-  // Generate a link to the users main photo:
+  // Generate a link to the users primary (first) photo:
   function generateLinkToMainPhoto(username) {
     var photoID;
-    for(var user of users) {
+    for(var user of users)
       if(user.Username === username) {
         photoID = user.images[0];
         break;
       }
-    }
     return `./pubImages/${username}/${photoID}.jpg`
   }
 
@@ -55,9 +53,22 @@ function Meet() {
     if(user.currentImage === user.images.length - 1)
       user.currentImage = 0;
     else
-      user.currentImage++
+      user.currentImage++;
     //Display the next image. The index of the next image is now the value of the currentImage property:
     document.getElementById(`main-pic-${user.Username}`).src = `./pubImages/${username}/${user.images[user.currentImage]}.jpg`;
+  }
+
+  function getAge(birthDay){
+    const milisecondsPerYear = 31536000000;
+    const milisecondsPerMonth = 2629800000;
+    var today = new Date();
+    var bday = new Date(birthDay);
+    var ageMiliseconds = Date.parse(today) - Date.parse(bday);
+  
+    if(ageMiliseconds >= milisecondsPerYear) //Older than 1 year.
+      return Math.floor(ageMiliseconds / milisecondsPerYear).toString() + "y";
+    else
+      return Math.floor(ageMiliseconds / milisecondsPerMonth).toString() + "m";
   }
 
   return (
@@ -68,12 +79,12 @@ function Meet() {
             <img className="cardprofilepic" src={generateLinkToMainPhoto(user.Username)} alt="Profile" />
             <div className="profilecontent">
                 <h1>{user.First_name}</h1>
-                <p>Age: </p><span>{user.Age}</span>
+                <p>Age: </p><span>{getAge(user.Birthdate)}</span>
                 <p>Sex: </p><span>{user.Sex} </span>
                 <p>Weight(lbs): </p><span>{user.Weight}</span>
                 <p>Breed: </p>  <span>{user.Breed}</span>
-                <img className="newsfeedpic" id={`main-pic-${user.Username}`} src={generateLinkToMainPhoto(user.Username)} alt="Profile" 
-                  onClick={() => showNextImage(user.Username)}/>
+                <img className="newsfeedpic" id={`main-pic-${user.Username}`} src={generateLinkToMainPhoto(user.Username)} 
+                alt="Profile" onClick={() => showNextImage(user.Username)}/>
                 <div className="reactions">
                   <img src={heart} height="64px" style={{padding:"0px 100px 0 0"}}/>
                   <img src={cross} height="72px"/>
